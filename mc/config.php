4cdf640354cbfec95ea72ded17a0eddfc3a704d3
fandri22 <52689800+fandri22@users.noreply.github.com>
2019-07-23 02:57:02 +0700

    Update config.php

--- mc/config.php
+++ mc/config.php
@@ -3,11 +3,11 @@ $user_agent = "com.breakcube.bc_1.1.15_17(Redmi 5;Android 7.1.2_25;in-ID)";
 
 //login
 $platform = "Android";
-$foreign_key = "xxxxxx";
-$sign = "xxxxx";
-$uuid = "xxxxx";
-$aid = "xxxxx";
-$android_id = "xxxx";
+$foreign_key = "4a41731cb20c7658";
+$sign = "4b21ee9913082d3bd2ab5dba7cfa080068d4d0a6";
+$uuid = "4f7526c51eea9d177a8c7ee2d9999d0260431dc4";
+$aid = "f85e8041-5487-4050-afb9-5b7444238867";
+$android_id = "4a41731cb20c7658";
 
 //click
 $setting_version = "8";
